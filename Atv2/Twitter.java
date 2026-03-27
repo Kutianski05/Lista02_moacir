9f03ed3e98c5472155717e776bc62e1fc413c072
João Vitor <engs-joaokutianski@camporeal.edu.br>
2026-03-27 19:42:08 -0300

    Atv2

---
+++ Atv2/Twitter.java
@@ -0,0 +1,13 @@
+package Atv2;
+
+class Twitter extends RedeSocial{
+    int twitch;
+    @Override
+    public void postar(String mensagem){
+        if(mensagem.length() < 280){
+            System.out.println("Tweetou " + mensagem);
+        } else {
+            System.out.println("Texto muito longo!! ");
+        }
+    }
+}
